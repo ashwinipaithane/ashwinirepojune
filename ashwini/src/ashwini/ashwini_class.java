4b88095ec170268fb8fcdba956911b3dfccdbb84
ashwi <ashwini paithane>
2023-06-08 23:53:12 +0530

    Project 1st commit on line no 14 (add extra comment)

---
+++ ashwini/src/ashwini/ashwini_class.java
@@ -0,0 +1,26 @@
+package ashwini;
+
+public class ashwini_class {
+
+	public static void main(String[] args) {
+		
+		
+		String str1="This is Automation";
+		String str2="";
+		char ch;
+		
+		int len=str1.length();
+		System.out.println(len);
+		//code by ashwini
+		
+		for(int i=str1.length()-1;i>=0;i--)
+		{
+			ch=str1.charAt(i);
+			str2=str2+ch;
+			
+		}
+		System.out.println(str2);
+
+	}
+
+}
